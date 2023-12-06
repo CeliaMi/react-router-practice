4fbef83ee00c1213f58e7309e30e831bf8577c3d
Celia <celiamillanalemany@gmail.com>
2023-12-06 20:00:12 +0100

    add text on About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <secction className='px-4 py-5 my-5 text-center'>
+    <h3 className='display-8 fw-bold text-body-emphasis'>What is this about?🤔</h3>
+    <p>this is a website where I practice new things on react router dom</p>
+  </secction>
   )
 }
 
